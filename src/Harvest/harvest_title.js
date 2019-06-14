3d5f05f81c8596f7213bfde67dcde07bb36a8423
ngorrepati <nikhilgx@umich.edu>
2019-06-14 01:10:54 -0500

    changed wording

--- src/Harvest/harvest_title.js
+++ src/Harvest/harvest_title.js
@@ -4,11 +4,11 @@ class Title extends React.Component {
   render() {
     return(
       <div>
-        <h1>What to Plant and Harvest Rice>?</h1>
+        <h1>When to Plant Rice>?</h1>
         <p>Displays whether or not the next 7 days are good days to plant rice depending on
           your location</p>
         <br>
-        <p>The ideal conditions for harvesting rice are </p>
+        <p>The ideal conditions for cultivating rice are </p>
       </div>
     );
   };
